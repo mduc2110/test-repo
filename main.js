9f3bddd778d9fb2f429c148e380fe2086e95d8f6
Andres Vu <62826110+mduc2110@users.noreply.github.com>
2022-06-25 17:01:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 console.log("Hello");
 
 console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
 
 function login() {
     let username = ""
